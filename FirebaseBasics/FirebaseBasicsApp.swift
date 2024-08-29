f26195a248974803a8066c6f44f0a1a84023dfe9
PascalAllekotte <pascalallekotte1@web.de>
2024-08-29 17:58:37 +0200

    Initial Commit

---
+++ FirebaseBasics/FirebaseBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBasicsApp.swift
+//  FirebaseBasics
+//
+//  Created by Pascal Allekotte on 29.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
