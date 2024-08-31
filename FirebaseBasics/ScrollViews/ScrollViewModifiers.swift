4c9413fa2e522ab8cf0fe773bedbaeff7e3bcd6a
PascalAllekotte <pascalallekotte1@web.de>
2024-09-01 00:04:58 +0200

    ScrollviewModifier

---
+++ FirebaseBasics/ScrollViews/ScrollViewModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewModifiers.swift
+//  FirebaseBasics
+//
+//  Created by Pascal Allekotte on 31.08.24.
+//
+
+import SwiftUI
+
+struct ScrollViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewModifiers()
+}
