f3009249fe61c4c4c81583e6f6b5be5e2d4c2a14
PascalAllekotte <pascalallekotte1@web.de>
2024-09-01 13:40:35 +0200

    Carousel

---
+++ FirebaseBasics/CarouselViews/LowerCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  LowerCarouselView.swift
+//  FirebaseBasics
+//
+//  Created by Pascal Allekotte on 01.09.24.
+//
+
+import SwiftUI
+
+struct LowerCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LowerCarouselView()
+}
